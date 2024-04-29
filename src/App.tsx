6eec55d2fd7d023996cdcd7c3cda298a57740b3d
kodaraj <aamirjamal619@gmail.com>
2024-04-29 17:22:43 +0530

    added text generator effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
+"use client";
 import { useState } from "react";
 import quotes from "./quotes.json";
 import TailwindcssButton from "./components/TailwindcssButtons";
+import { TextGenerateEffect } from "./components/TextGenerateEffect";
+// import { TextGenerateEffectDemo } from "./components/TextGenerateEffectDemo";
 
 function App() {
   const [randomQuote, setRandomQuote] = useState(() => {
     return quotes[Math.floor(Math.random() * quotes.length)];
   });
-
+  console.log(randomQuote);
   return (
-    <div className="flex flex-col h-full justify-around items-center gap-10">
+    <div className="flex flex-col h-full justify-around items-center gap-12">
       <div className="flex flex-1 items-end text-center">
         <div>
-          <h1 className="text-5xl font-dancingScript my-4">
-            {randomQuote.split("-")[0]}
+          <h1 className="text-4xl font-dancingScript my-0">
+            <TextGenerateEffect words={randomQuote} />
           </h1>
-          <p className="text-xl">- {randomQuote.split("-")[1]}</p>
         </div>
       </div>
       <div className="flex-1 mb-auto">
--- src/components/TailwindcssButtons.tsx
+++ src/components/TailwindcssButtons.tsx
@@ -3,7 +3,6 @@
 export default function TailwindcssButton({ setRandomQuote, quotes }) {
   function quoteHandler() {
     setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)]);
-    console.log("ffwwfe");
   }
   return (
     <button
@@ -11,7 +10,7 @@ export default function TailwindcssButton({ setRandomQuote, quotes }) {
       className="px-8 py-2 my-2 border border-black bg-transparent #141e46  dark:border-white relative group transition duration-200 rounded-lg"
     >
       <div className="absolute -bottom-2 -right-2 bg-yellow-300 h-full w-full -z-10 group-hover:bottom-0 group-hover:right-0 transition-all duration-200 rounded-lg" />
-      <span className="relative">GENERATE</span>
+      <span className="relative text-lg">Generate</span>
     </button>
   );
 }
